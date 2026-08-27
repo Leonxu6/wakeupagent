11aee3f624454283c6e0900f04c8d1fe3f022ac0
Leon Xu <leonxuu@outlook.com>
2026-08-27 17:13:51 +0800

    test(maintenance): lock reliability audit registration

--- tests/test_maintenance_security_registration.py
+++ tests/test_maintenance_security_registration.py
@@ -16,3 +16,24 @@ def test_security_and_syntax_audits_are_registered():
         "markdown_fence_audit.py",
     }
     assert expected.issubset(set(_AUDITS))
+
+
+def test_reliability_and_async_audits_are_registered():
+    expected = {
+        "bare_except_audit.py",
+        "silent_exception_audit.py",
+        "mutable_default_audit.py",
+        "wildcard_import_audit.py",
+        "debug_call_audit.py",
+        "os_system_audit.py",
+        "datetime_utcnow_audit.py",
+        "absolute_user_path_audit.py",
+        "duplicate_definition_audit.py",
+        "unsafe_chmod_audit.py",
+        "http_timeout_audit.py",
+        "urlopen_timeout_audit.py",
+        "subprocess_timeout_audit.py",
+        "async_blocking_sleep_audit.py",
+        "async_subprocess_audit.py",
+    }
+    assert expected.issubset(set(_AUDITS))
